cfa6993ddb37a91d94093bb9424160028f0b0e5d
guillot bertrand <guillotbertrand95@gmail.com>
2025-03-14 09:00:09 +0100

    premier commit

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = [
+	"Pas de Panique",
+	"La vie, l'univers et le reste",
+	"Merci pour le poisson",
+];
+//variable globale
